04f26c02f5481b65e462fc66818fbc58cdd922ef
sw <chongde.sw@alibaba-inc.com>
2024-03-25 03:28:34 +0800

    Fix

--- jdk/src/solaris/native/java/lang/childproc.h
+++ jdk/src/solaris/native/java/lang/childproc.h
@@ -120,7 +120,7 @@ typedef struct _SpawnInfo {
  * The cached and split version of the JDK's effective PATH.
  * (We don't support putenv("PATH=...") in native code)
  */
-const char * const *parentPathv;
+extern const char * const *parentPathv;
 
 
 ssize_t restartableWrite(int fd, const void *buf, size_t count);
